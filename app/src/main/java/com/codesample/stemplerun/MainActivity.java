b3c5a346530206bd6b05b7236244c05643e3d3fe
Roger752 <polo9562@gmail.com>
2020-06-15 20:25:47 +0900

    일정 좌표값 overlay 타원 생성, 일정 좌표값 마커 지정

--- app/src/main/java/com/codesample/stemplerun/MainActivity.java
+++ app/src/main/java/com/codesample/stemplerun/MainActivity.java
@@ -5,17 +5,19 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.view.ViewGroup;
 
 import com.codesample.stemplerun.databinding.ActivityMainBinding;
 
 
+import net.daum.mf.map.api.CameraUpdateFactory;
 import net.daum.mf.map.api.MapCircle;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPointBounds;
 import net.daum.mf.map.api.MapView;
 
 
@@ -25,43 +27,14 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
     private MapView mapView;
     private MapPOIItem marker;
     private MapCircle mapCircle;
+    private ViewGroup mapViewContainer;
+    private MapPoint mapPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        mapView = new MapView(this);
-        mapView.setPOIItemEventListener(this);
-
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
-
-        marker = new MapPOIItem();
-        marker.setMapPoint(mapView.getMapCenterPoint());
-        marker.setItemName("Me");
-        marker.setTag(0);
-        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
-        mapView.addPOIItem(marker);
-
-        // 트래킹 모드 On, 나침반 기능 Off
-        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
-
-        // Generate Kakao Hash Key
-        /*try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md;
-                md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                String something = new String(Base64.encode(md.digest(), 0));
-                Log.e("Hash key", something);
-            }
-        } catch (Exception e) {
-            Log.e("name not found", e.toString());
-        }*/
 
         //권한 체크
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
@@ -77,6 +50,65 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
             }
         }
 
+        mapView = new MapView(this);
+        mapView.setPOIItemEventListener(this);
+
+        mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+
+        // 트래킹 모드 On, 나침반 기능 Off
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+        mapView.setShowCurrentLocationMarker(true);
+        Log.i("SHOWING", mapView.isShowingCurrentLocationMarker() + "");
+
+        // Generate Kakao Hash Key
+        /*try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            Log.e("name not found", e.toString());
+        }*/
+
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(35.893244, 128.621894));
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
+
+        MapCircle circle1 = new MapCircle(
+                MapPoint.mapPointWithGeoCoord(35.893098, 128.621687), // center
+                10, // radius
+                Color.argb(128, 255, 0, 0), // strokeColor
+                Color.argb(128, 0, 255, 0) // fillColor
+        );
+        circle1.setTag(1234);
+        mapView.addCircle(circle1);
+
+        MapCircle circle2 = new MapCircle(
+                MapPoint.mapPointWithGeoCoord(35.896280, 128.622049), // center
+                1000, // radius
+                Color.argb(128, 255, 0, 0), // strokeColor
+                Color.argb(128, 255, 255, 0) // fillColor
+        );
+        circle2.setTag(5678);
+//        mapView.addCircle(circle2);
+
+// 지도뷰의 중심좌표와 줌레벨을 Circle이 모두 나오도록 조정.
+        MapPointBounds[] mapPointBoundsArray = { circle1.getBound(), circle2.getBound() };
+        MapPointBounds mapPointBounds = new MapPointBounds(mapPointBoundsArray);
+        int padding = 50; // px
+        mapView.moveCamera(CameraUpdateFactory.newMapPointBounds(mapPointBounds, padding));
+
     }
 
     @Override
@@ -89,14 +121,11 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
 
         marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
         marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
         mapView.addPOIItem(marker);
         Log.i("MAIN", "marker : " + marker);
-
-
-
     }
 
-    // 마커 Override
     @Override
     public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
 
@@ -112,6 +141,7 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
 
     }
 
+    // POIItem 리스너
     @Override
     public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
 
